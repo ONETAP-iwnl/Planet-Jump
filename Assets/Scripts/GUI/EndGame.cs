c9ddffaa20d35f2d750d7efa28716d47769f96c8
BesideUwU <roma_yusupov_05@mail.ru>
2023-08-31 22:52:56 +0500

    chore!: Прочитайте текстовый файл README
    Я нихуя не сделал, пытался сделать взаимодействие с метеоритом, когда соприкасается то, корабль уничтожается.

---
+++ Assets/Scripts/GUI/EndGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Meteor"))
+        {
+            BreakRocket();
+        }
+    }
+
+    public void BreakRocket()
+    {
+        Destroy(gameObject);
+    }
+}
